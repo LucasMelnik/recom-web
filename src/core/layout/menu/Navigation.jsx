91c8a80444ef7a0f227b0801051343481030ebb2
Lucas Melnik <lucas_melnik.99@hotmail.com>
2022-06-02 02:28:50 -0300

    integration with api

--- src/core/layout/menu/Navigation.jsx
+++ src/core/layout/menu/Navigation.jsx
@@ -1,13 +1,18 @@
-import { Space } from "antd";
+import { useContext } from "react";
+import { Button, Space } from "antd";
 import { Link } from "react-router-dom";
+import { Context } from "../../../context/AuthContext";
 
 export default function Navigation() {
+  const { handleLogout } = useContext(Context)
+
   return (
     <Space size='large'>
       <Link to="/orders">Pedidos</Link>
       <Link to="/customers">Clientes</Link>
       <Link to="/factories">Fábricas</Link>
       <Link to="/products">Produtos</Link>
+      <Button onClick={handleLogout}>Logout</Button>
     </Space>
   )
 }
--- src/pages/order/OrderList.jsx
+++ src/pages/order/OrderList.jsx
@@ -1,13 +1,20 @@
 import { Button, Space } from "antd";
 import { Link } from "react-router-dom";
+import api from "../../config/api";
 
 export default function OrderList() {
+
+  async function getUsers() {
+  const users = await api.get('/users')
+  console.log("🚀 ~ file: OrderList.jsx ~ line 9 ~ getUsers ~ users", users)
+  }
+
   return (
     <Space size="small">
       <Button type="primary">
         <Link to="/order/new">Novo Pedido</Link>
       </Button>
-      <Button>Relatório</Button>
+      <Button onClick={getUsers}>Relatório</Button>
     </Space>
   );
 }
