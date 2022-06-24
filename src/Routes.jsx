b72acee2e486ea4b3f068bab036fc94fba858cd9
bluexbaez <bluexbaez@gmail.com>
2022-06-24 17:07:25 -0300

    corrigido erro de merge

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,12 +5,9 @@ import { Context } from './context/AuthContext'
 import FactoryList from './pages/factory/FactoryList.jsx'
 import OrderItems from './pages/order/OrderItems'
 import OrderList from './pages/order/OrderList'
-<<<<<<< HEAD
 import CreateProduct from './pages/product/CreateProduct'
 import ProductList from './pages/product/ProductList'
 // import { CreateFactory } from './pages/factory/CreateFactory.jsx'
-=======
->>>>>>> 00c5ca19b869e660d44067398cf9b999d8d639ac
 
 export default function Routes() {
   const { loading } = useContext(Context)
